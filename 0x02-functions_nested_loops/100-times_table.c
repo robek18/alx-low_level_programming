4a399f11fe8317efb09f0c61a70d96cf03244f68
robel ketema <robel18.ketema@gmail.com>
2022-09-13 23:30:41 +0000

    0x02

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: table size
+ * Return: table
+ */
+void print_times_table(int n)
+{
+
+}
